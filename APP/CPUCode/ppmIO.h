10b041aa04acc165bee7e234e4d7efb988a71955
krbezlija9 <krbezlijam9@gmail.com>
2018-11-20 21:10:37 +0100

    EdgeDetection

---
+++ APP/CPUCode/ppmIO.h
@@ -0,0 +1,13 @@
+void loadImage(
+	char *filename,
+	int **dest,
+	int *width,
+	int *height,
+	int grayscale);
+
+void writeImage(
+	char *filename,
+	int *data,
+	int width,
+	int height,
+	int grayscale);
